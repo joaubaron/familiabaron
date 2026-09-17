1a6aa8e879878e34940ab5fd1b9abbe5328b66f9
joaubaron <joaubaron@users.noreply.github.com>
2026-09-17 16:08:54 +0000

    deploy: 5615a81cc925b102eb85be03a2572be960dc2ac9

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker
 // A versão é atualizada automaticamente pelo deploy.yml a cada push no GitHub Pages.
-const CACHE_VERSION = '17.09.2026-1052';
+const CACHE_VERSION = '17.09.2026-1308';
 const CACHE_NAME = `album-baron-${CACHE_VERSION}`;
 
 const CORE = [
